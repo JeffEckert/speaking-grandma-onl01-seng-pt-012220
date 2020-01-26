a525a526bcafcc92b2e75a62a9cc8fc75db31c05
Jeffery Eckert <bromodo@gmail.com>
2020-01-26 03:45:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,12 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(phrase)
+  if #{phrase} == #{phrase}.upcase
+  puts
+  "NO, NOT SINCE 1938!"
+  end
+end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
