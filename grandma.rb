f7d9e1d0edf4d64a954597b1541f557ef5be703a
Jeffery Eckert <bromodo@gmail.com>
2020-01-26 03:59:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,9 @@ def speak_to_grandma(phrase)
   end
     if phrase != phrase.upcase()
       return "HUH? SPEAK UP, SONNY!"
+    else
+      return "NO, NOT SINCE 1938!"
+    end
 end
 
 # Whatever you say to grandma, she should respond with
