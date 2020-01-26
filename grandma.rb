8985158b889bf422dc9cde02f926ff976dbc822f
Jeffery Eckert <bromodo@gmail.com>
2020-01-26 03:58:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,7 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
   end
+    if phrase != phrase.upcase
 end
 
 # Whatever you say to grandma, she should respond with
